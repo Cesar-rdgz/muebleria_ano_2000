a5f1aa1c0b30c79747e5476087ec0230c36e02de
Cesar Rodriguez <cesarrdgz.18.95@gmail.com>
2020-10-03 20:43:20 -0500

    more improvements

--- js/main.js
+++ js/main.js
@@ -2,38 +2,39 @@ let sliderCards = document.querySelectorAll('.slide-content'),
     arrowLeft = document.querySelector('#arrow-left'),
     arrowRight = document.querySelector('#arrow-right'),
     current = 0
-    animation = document.querySelectorAll('.slide');
+    animation = document.querySelectorAll('#image-ani');
 
 
 // hidde everything
 function reset() {
-    for( let i = 0; i < animation.length; i++) {
-        animation[i].style.display = 'none';
+    for( let i = 0; i < sliderCards.length; i++) {
+        sliderCards[i].style.display = 'none';
     }
 }
 
 // initializer
 function startSlide() {
     reset();
-    animation[0].style.display = 'block';
+    sliderCards[0].style.display = 'block';
 }
 
 // move to left
 function slideLeft () {
     reset();
-    animation[current - 1].style.display = 'block';
+    sliderCards[current - 1].style.display = 'block';
     current--;
     animation[current].classList.remove('animateRight');
     animation[current].classList.add('animateLeft');
 }
 
 //internval for the slider
-setInterval(slideRight, 5000);
+//setInterval(slideRight, 5000);
 
 // move to right
 function slideRight() {
     reset();
-    animation[current + 1].style.display = 'block';
+    sliderCards[current + 1].style.display = 'block';
+    // las animaciones tiene ir en el contenido, y las imagenes dejar de ser background
     current++;
     animation[current].classList.remove('animateLeft');
     animation[current].classList.add('animateRight'); 
@@ -45,14 +46,14 @@ function slideRight() {
 //click left arrow
 arrowLeft.addEventListener('click', function (){
     if(current === 0){
-        current = animation.length;
+        current = sliderCards.length;
     }
     slideLeft();
 });
 
 //click right arrow
 arrowRight.addEventListener('click', function (){
-    if(current === animation.length -1){
+    if(current === sliderCards.length -1){
         current = -1;
     }
     slideRight();
