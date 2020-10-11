a3408866d11e667f618ea1e1e225961dd839bbfe
Cesar Rodriguez <cesarrdgz.18.95@gmail.com>
2020-10-10 19:34:40 -0500

    more errors

--- sw.js
+++ sw.js
@@ -1,14 +1,12 @@
 const staticCacheName = 'site-static';
-/*const assets = [
+const assets = [
     "index.html",
     "css/styles.css",
     "css/all.css",
     "js/app.js",
     "js/main.js",
     "/",
-    "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css",
-    "images/logo-company-muebleria.jpg",
-];*/
+];
 
 // install service worker
 self.addEventListener('install', evt => {
@@ -16,7 +14,7 @@ self.addEventListener('install', evt => {
     evt.waitUntil(
         caches.open(staticCacheName).then(cache => {
             console.log('caching shell assets');
-            cache.addAll(["index.html", "css/styles.css", "css/all.css", "js/app.js", "js/main.js", "/", "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css", "images/logo-company-muebleria.jpg",]);
+            cache.addAll(assets);
         })
     );
 });
