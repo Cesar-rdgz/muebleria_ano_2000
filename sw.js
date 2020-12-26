9dd5a7be36b89eceb536395a1da365b8584d2078
Cesar Rodriguez <cesarrdgz.18.95@gmail.com>
2020-12-26 14:42:19 -0600

    more layout improvements

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'site-static-v1.4.9';
-const dynamicCacheName = 'site-dynamic-v1.4.9';
+const staticCacheName = 'site-static-v1.5.3';
+const dynamicCacheName = 'site-dynamic-v1.5.3';
 const assets = [
     "index.html",
     "css/styles.css",
